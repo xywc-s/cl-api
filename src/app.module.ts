b0b01c0fae17c098a96af3bd515aee01d6b7360a
易长松 <xywc_s@foxmail.com>
2021-01-29 14:45:29 +0800

    config

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,17 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { DbsModule } from './dbs/dbs.module';
 
 @Module({
-  imports: [DbsModule],
+  imports: [
+    DbsModule,
+    ConfigModule.forRoot({
+      expandVariables: true,
+      isGlobal: true
+    })
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
--- src/dbs/dbs.provider.ts
+++ src/dbs/dbs.provider.ts
@@ -1,18 +1,23 @@
+import { ConfigService } from '@nestjs/config';
 import { MongoClient } from 'mongodb';
-
-const uri = 'mongodb://etfun:Vzgc7tJ3@localhost:27017/mercador'
-
-const options = {
-  useUnifiedTopology: true,
-  authSource: 'mercador'
-}
 
 export const DB = [{
   provide: 'db',
-  useFactory: async () => {
-    const connect = await MongoClient.connect(uri, options)
+  useFactory: async (config: ConfigService) => {
+    const host = config.get<string>('MongoDB_HOST', 'localhost')
+    const port = config.get<number>('MongoDB_PORT', 27017)
+    const user = config.get<string>('MongoDB_USER')
+    const secret = config.get<string>('MongoDB_SECRET')
+    const db = config.get<string>('MongoDB_DB','admin')
+    const uri = `mongodb://${user}:${secret}@${host}:${port}/${db}`
+    const client = new MongoClient(uri,{
+      useUnifiedTopology: true,
+      authSource: db
+    })
+    const connect = await client.connect()
     return connect.db()
-  }
+  },
+  inject: [ConfigService]
 },{
   provide: 'redis',
   useFactory: ()=>{
